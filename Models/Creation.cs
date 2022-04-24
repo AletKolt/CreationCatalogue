a6bd9f446e668d6cdad19156ffba14ff2a95a6cd
Aletan Kindness O <aletkolt@gmail.com>
2022-04-24 22:08:23 +0100

    Creation and CreationCategory models added to the project

---
+++ Models/Creation.cs
@@ -0,0 +1,10 @@
+namespace CreationCatalogue.Models
+{
+    public class Creation
+    {
+        private int CreationId {get; set;}
+        private string CreationName {get; set;}
+        private int CategoryId {get; set;}
+        private string Detail {get; set;}
+    }
+}
---
+++ Models/CreationCategory.cs
@@ -0,0 +1,8 @@
+namespace CreationCatalogue.Models
+{
+    public class CreationCategory
+    {
+        private int CategoryId {get; set;}
+        private string Category {get; set;}
+    }
+}
